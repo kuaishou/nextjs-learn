61311b65fb3835ccefc6ee7b03da56c1f6b2b0a6
Haodong1 <xinghd>
2024-03-06 14:48:26 +0800

    feat: add mate date

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,9 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: 'customers',
+};
+
 export default function Page() {
   return <p>http://localhost:3000/dashboard/customers Page</p>;
 }
